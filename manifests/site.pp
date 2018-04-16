4addea7597be4d276e75a1b5019c8ccf69672754
abarnup <barwat.anup@gmail.com>
2018-04-15 18:43:46 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,8 @@
-   node default {
-    file {'/root/README':	    file {'/root/README':
-       ensure => file,	       ensure => file,
--      content => 'This is a readme',	+      content => 'Ashwini',
-       owner => root,	       owner => root,
-    }	    
+node default {
+ }	    
  
- node 'master.puppet.vm'{
+node 'master.puppet.vm'{
       include role::master_server
-      
 }
-   user { 'tommy':
-       ensure => present
-    }
-    
-    file {'/root/anupfile':
-      ensure => file,
-      content => 'sklfskdjsdkjsdjsfjdksjfsdfsdf ',
-      owner => root,
-   }
+ 
   
-}
